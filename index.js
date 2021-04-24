1a9ee85f044e026d44962c85a9e8a950ed62324b
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2021-04-24 17:30:58 -0400

    Added Cool Background Color Change Mechanic

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+// Get movies made by Davids using imdb api
 async function getDavids() {
     let response = await fetch('https://api.themoviedb.org/3/movie/550?api_key=0280dba172ca6d48aca63dc2fd6df2be/person?query=david');
     let data = await response.json()
@@ -6,3 +7,15 @@ async function getDavids() {
 }
 
 getDavids().then(data => console.log(data));
+
+// Cool background color change for *aesthetics*
+var i = 0;
+
+function updateBackground() {
+  let doc = document.getElementById("background");
+  let color = ["#faf884", "powderblue", "#f8b88b", "#baed91"];
+  doc.style.backgroundColor = color[i];
+  i = (i + 1) % color.length;
+}
+
+setInterval(updateBackground, 1000);
